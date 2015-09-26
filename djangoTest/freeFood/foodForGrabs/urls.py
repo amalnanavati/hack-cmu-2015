f8927d16fe76770f28630c3e18e14a4047c05b2f
Anna Gupta <AnnaGupta@Anna-Guptas-MBP.wv.cc.cmu.edu>
2015-09-26 16:38:04 -0400

    Deleted unnecessary files

--- djangoTest/freeFood/foodForGrabs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<event_id>[0-9]+)/vote/$', views.detail, name='detail'),
-    ]
